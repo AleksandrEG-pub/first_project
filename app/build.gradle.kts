ed9f842aee2f10fc5c8a80757fecef183de3e4fc
aleksandr <aleksandr.e.gusev@gmail.com>
2025-11-16 11:42:38 +0300

    add mockito dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,12 +12,14 @@ dependencies {
     implementation("org.postgresql:postgresql:42.7.8")
 
     testImplementation("org.testcontainers:testcontainers:2.0.2")
+    testImplementation("org.testcontainers:testcontainers-junit-jupiter:2.0.2")
     testImplementation("org.testcontainers:postgresql:1.21.3")
+
     testImplementation(platform("org.junit:junit-bom:6.0.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter")
-    testImplementation("org.assertj:assertj-core:3.24.2")
+
     testImplementation("org.assertj:assertj-core:4.0.0-M1")
+    testImplementation("org.mockito:mockito-core:5.20.0")
 }
 
 java {
