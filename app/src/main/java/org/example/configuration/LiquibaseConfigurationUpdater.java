e134b871f7d492590b4606b65cac039ab358eeaa
aleksandr <aleksandr.e.gusev@gmail.com>
2025-11-28 15:24:33 +0300

    move liquibase to spring properties

--- app/src/main/java/org/example/configuration/LiquibaseConfigurationUpdater.java
+++ app/src/main/java/org/example/configuration/LiquibaseConfigurationUpdater.java
@@ -11,32 +11,44 @@ import liquibase.database.jvm.JdbcConnection;
 import liquibase.exception.LiquibaseException;
 import liquibase.resource.ClassLoaderResourceAccessor;
 import org.example.exception.InitializationException;
+import org.springframework.beans.factory.annotation.Value;
 
 public class LiquibaseConfigurationUpdater {
 
-  private final LiquibaseConfiguration liquibaseConfiguration;
+  /** liquibase configuration file location */
+  @Value("${database.migration.liquibase.changelog_file}")
+  private String changelogFile;
 
-  public LiquibaseConfigurationUpdater(LiquibaseConfiguration liquibaseConfiguration) {
-    this.liquibaseConfiguration = liquibaseConfiguration;
-  }
+  /** url of database to connect */
+  @Value("${database.connect.url}")
+  private String url;
+
+  /** user to connect to database */
+  @Value("${database.connect.user}")
+  private String username;
+
+  /** password to connect to database */
+  @Value("${database.connect.password}")
+  private String password;
+
+  /** name of database scheme with liquibase tables */
+  @Value("${database.migration.liquibase.scheme}")
+  private String liquibaseScheme;
+
+  /** name of database scheme with application tables and data */
+  @Value("${database.migration.liquibase.application_scheme}")
+  private String applicationScheme;
 
   public void runDatabaseUpdate(String context) {
-    try (Connection connection =
-        DriverManager.getConnection(
-            liquibaseConfiguration.getUrl(),
-            liquibaseConfiguration.getUsername(),
-            liquibaseConfiguration.getPassword())) {
+    try (Connection connection = DriverManager.getConnection(url, username, password)) {
       createSchemas(connection);
       Database database =
           DatabaseFactory.getInstance()
               .findCorrectDatabaseImplementation(new JdbcConnection(connection));
-      database.setLiquibaseSchemaName(liquibaseConfiguration.getLiquibaseScheme());
-      database.setDefaultSchemaName(liquibaseConfiguration.getApplicationScheme());
+      database.setLiquibaseSchemaName(liquibaseScheme);
+      database.setDefaultSchemaName(applicationScheme);
       Liquibase liquibase =
-          new Liquibase(
-              liquibaseConfiguration.getChangelogFile(),
-              new ClassLoaderResourceAccessor(),
-              database);
+          new Liquibase(changelogFile, new ClassLoaderResourceAccessor(), database);
       liquibase.update(context);
     } catch (LiquibaseException | SQLException e) {
       System.err.println("Failed to update database: " + e.getMessage());
@@ -46,12 +58,10 @@ public class LiquibaseConfigurationUpdater {
 
   private void createSchemas(Connection connection) throws SQLException {
     try (Statement stmt = connection.createStatement()) {
-      stmt.executeUpdate(
-          "CREATE SCHEMA IF NOT EXISTS " + liquibaseConfiguration.getLiquibaseScheme());
+      stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS " + liquibaseScheme);
     }
     try (Statement stmt = connection.createStatement()) {
-      stmt.executeUpdate(
-          "CREATE SCHEMA IF NOT EXISTS " + liquibaseConfiguration.getApplicationScheme());
+      stmt.executeUpdate("CREATE SCHEMA IF NOT EXISTS " + applicationScheme);
     }
   }
 }
