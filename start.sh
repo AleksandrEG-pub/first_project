6bfbe2c0b2e29329b33328624b7882157905904d
aleksandr <aleksandr.e.gusev@gmail.com>
2025-12-06 20:30:43 +0300

    update readme and start script

--- start.sh
+++ start.sh
@@ -2,8 +2,4 @@
 
 ./start-database.sh
 
-./gradlew :loggin:publishToMavenLocal --no-configuration-cache
-./gradlew :database-connector:publishToMavenLocal --no-configuration-cache
-./gradlew :audit:publishToMavenLocal --no-configuration-cache
-
-#./gradlew shadowJar && java -jar ./app/build/libs/app-all.jar --file=application.env
+./gradlew :app:bootRun
